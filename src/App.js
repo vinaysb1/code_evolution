05fb3b4173f02dd5d73fc35dbf2a1aee17e30657
vinay <vinaykumarbeelagi@gmail.com>
2023-04-18 22:11:36 +0530

    methods and props(parent to child

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Counter from './Components/Counter';
 import FunctionClick from './Components/FunctionClick';
 import ClassClick from './Components/ClassClick';
 import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponent />
+      {/* <EventBind /> */}
       {/* <FunctionClick/>
       <ClassClick/> */}
       {/* <Counter/> */}
